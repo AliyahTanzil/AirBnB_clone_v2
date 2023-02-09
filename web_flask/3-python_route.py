fc48d567c27daf5a4d809bfbdc320f56bcc64b93
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2023-02-09 10:32:48 +0000

    Web Flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -12,16 +12,19 @@ def index():
         """returns Hello HBNB!"""
         return 'Hello HBNB!'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """display "C" followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def ppythoniscool(text='is cool'):
     """display "Python", follwed by the value of the text variable"""
     return 'Python' + text.replace('_', ' ')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
