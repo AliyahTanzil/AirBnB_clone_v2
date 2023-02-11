4684b0d0ed3c09e74585076ce29f51d882490cd2
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2023-02-11 03:00:19 +0000

    Number Route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -12,8 +12,7 @@ def index():
         """returns Hello HBNB!"""
         return 'Hello HBNB!'
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
@@ -26,27 +25,10 @@ def cisfun(text):
     """display “C ” followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
 
-=======
-=======
-
->>>>>>> fc48d567c27daf5a4d809bfbdc320f56bcc64b93
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-        """returns HBNB"""
-        return 'HBNB'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-        """display “C ” followed by the value of the text variable"""
-        return 'C ' + text.replace('_', ' ')
->>>>>>> ef3c7e2619eccc55ae67e961455ac3f0408bf41d
-
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
-<<<<<<< HEAD
     """display “Python ”, followed by the value of the text variable"""
     return 'Python ' + text.replace('_', ' ')
 
@@ -58,16 +40,3 @@ def imanumber(n):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
-
-=======
-        """display “Python ”, followed by the value of the text variable"""
-        return 'Python ' + text.replace('_', ' ')
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def imanumber(n):
-        """display “n is a number” only if n is an integer"""
-        return "{:d} is a number".format(n)
-
-if __name__ == '__main__':
-        app.run(host='0.0.0.0', port='5000')
->>>>>>> ef3c7e2619eccc55ae67e961455ac3f0408bf41d
